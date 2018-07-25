8ef1d57e827291060076e94a2b8da09246c18916
Mjollnir93 <39071273+Mjollnir93@users.noreply.github.com>
2018-07-25 15:50:20 -0700

    Add files via upload

--- trackster.js
+++ trackster.js
@@ -1,16 +1,25 @@
-$(document).ready()
+var Trackster = {};
+const API_KEY = '0c9fada55cecff54e6d6f6664a0e3ccf'
 
-$('#search-input').click(function()  {
-  
+$(document).ready(function() {
+  $('#search-button').click(function() {
+    Trackster.searchTracksByTitle($('#search-input').val());
+  });
 });
 
-var Trackster = {};
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
+
+
+     for (var trackIndex = 0; trackIndex < length; trackIndex++) {
+
+       
+
+     }
 
 };
 
@@ -19,5 +28,7 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+  $.ajax( {
+    url:'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+    })
 };
