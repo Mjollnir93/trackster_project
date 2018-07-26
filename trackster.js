5deefb7b83cca1b665d4df1a307169eb810a1cc5
Mjollnir93 <39071273+Mjollnir93@users.noreply.github.com>
2018-07-26 16:04:13 -0700

    Add files via upload

--- trackster.js
+++ trackster.js
@@ -7,27 +7,25 @@ $(document).ready(function() {
   });
 });
 
-
-/*
-  Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks.
-*/
 Trackster.renderTracks = function(tracks) {
 
+    var $trackList = $('#track-list');
+    $trackList.empty();
 
-     for (var trackIndex = 0; trackIndex < length; trackIndex++) {
-
+     for (var trackIndex = 0; trackIndex < tracks.length; trackIndex++) {
+        var track =tracks[trackIndex];
+        var mediumAlbumArt = track.image[1]["#text"];
         var htmlTrackRow =
         '<div class="row track">'+
          '<div class="col-xs-1 col-xs-offset-1 play-button">'+
-           '<a href="https://youtu.be/eI_O5_tJ1hA" target="_blank">'+
+           '<a href="'+ track.url + '" target="_blank">' +
             '<i class="fa fa-play-circle-o fa-2x"></i>'+
             '</a>'+
             '</div>'+
-            '<div class="col-xs-4">' + tracks[i].title + '</div>'+
-            '<div class="col-xs-2">' + tracks[i].artist + '</div>'+
-            '<div class="col-xs-2">' + tracks[i].title + '</div>'+
-            '<div class="col-xs-2">' + tracks[i].listeners + '</div>'+
+            '<div class="col-xs-4">' + track.name + '</div>'+
+            '<div class="col-xs-2">' + track.artist + '</div>'+
+            ' <div class="col-xs-2"><img src="' + mediumAlbumArt + '"/></div>'+
+            '<div class="col-xs-2">' + track.listeners + '</div>'+
             '</div>';
 
             $trackList.append(htmlTrackRow);
@@ -35,12 +33,12 @@ Trackster.renderTracks = function(tracks) {
 
 };
 
-/*
-  Given a search term as a string, query the LastFM API.
-  Render the tracks given in the API query response.
-*/
 Trackster.searchTracksByTitle = function(title) {
-  $.ajax( {
-    url:'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
-    })
+  $.ajax({
+    url:'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title
+    + '&api_key=' + API_KEY + '&format=json',
+    success: function(response) {
+      Trackster.renderTracks(response.results.trackmatches.track);
+    }
+  });
 };
